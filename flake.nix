b0fde39cab89e3ec107ac63115abb55d522f894d
Claudio Bley <claudio.bley@tweag.io>
2024-11-08 12:42:30 +0100

    Use nix

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "Reveal-md slides";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-24.05";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      packages.${system}.default = pkgs.nodePackages.reveal-md;
+    };
+}
